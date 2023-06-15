98d57d7ef7482d324261d18231aa1cd79c7e2856
vannalyc <126526667+vannalyc@users.noreply.github.com>
2023-06-14 23:17:56 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import './App.css'
-import { puppyList } from './data'
-import { useState } from 'react'
-import './App.css'
-import './index.css'
-
+import './App.css';
+import { puppyList } from './data';
+import { useState } from 'react';
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
 
@@ -15,30 +11,40 @@ function App() {
 
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log(featuredPup);
- 
+
   return (
     <div className="App">
+      {featPupId && (
+        <div className='featuredPup'>
+          <h2 className='feat-header'>Featured Puppy</h2>
+          <h3 className='feat-header2'>{featuredPup.name}</h3>
+          <ul>
+            <li className='no-bullet'>Age: {featuredPup.age}</li>
+            <li className='no-bullet'>Email: {featuredPup.email}</li>
+            <li className='no-bullet'>Tricks:</li>
+            <ul>
+              {featuredPup.tricks.map((trick) => (
+                <li key={trick.id}>{trick.title}</li>
+              ))}
+            </ul>
+          </ul>
+        </div>
+      )} 
+      <div className='puppy-pals'>
+      <h1> Puppy Pals</h1>
       {puppies.map((puppy) => (
         <p
           key={puppy.id}
           onClick={() => {
-            setFeatPupId(puppy.id); 
+            setFeatPupId(puppy.id);
           }}
         >
           {puppy.name}
         </p>
       ))}
-      {featPupId && (
-        <div>
-          <h2>{featuredPup.name}</h2>
-          <ul>
-            <li>Age: {featuredPup.age}</li>
-            <li>Email: {featuredPup.email}</li>
-          </ul>
-        </div>
-      )}
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
